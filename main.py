bf2247af13e4f10006d07c0c9ae440b5e0e02dbf
Xuyezhou <yezhou.xu@my.jcu.edu.au>
2021-08-02 22:44:38 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Author:Xuyezhou
+Date:30 jul 2021
+Description:
+This is a demo of pycharm with Git and Github
+Ensure you have a Github account
+"""
+def main()：
+    """Start the program"""
+    print("Hello,my name is Xu Yezhou")
+
+
+if __name__ == '__main__':
+    main()
